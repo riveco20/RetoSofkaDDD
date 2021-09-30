28ba517b00e79fa298721ede465279291d384d05
Richard Vellojin Coneo <richardvellojin@gmail.com>
2021-09-29 23:28:24 -0500

    Agregando archivo al repositorio

---
+++ src/main/java/com/sofka/domainPaqueExplora/DomainPaqueExploraApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.domainPaqueExplora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainPaqueExploraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainPaqueExploraApplication.class, args);
+	}
+
+}
